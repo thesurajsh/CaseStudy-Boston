a2ccae858202d1052cae990c2d2f67f596eb9dbd
Suraj  Shelar <83068425+thesurajsh@users.noreply.github.com>
2023-12-05 18:40:05 +0530

    Create Boston.py

---
+++ Boston.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from sklearn.metrics import mean_squared_error
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+import seaborn as sns
+
+data = pd.read_csv('BostonHousing.csv')
+
+linr = LinearRegression()
+
+data['medv'] = np.log1p(data['medv'])
+
+x = data.drop(['medv','b'], axis=1)
+y = data['medv']
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, random_state=40, test_size=0.2)
+linr.fit(x_train, y_train)
+y_pred = linr.predict(x_test)
+print(mean_squared_error(y_test, y_pred))
+
+
+# Mean Squared Error:  0.031129333980953557
